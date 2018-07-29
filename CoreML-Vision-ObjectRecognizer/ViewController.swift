0aba463109a38c305027b27ac70262b3952e3150
Adi <aditya.chinchure@gmail.com>
2018-07-29 14:03:50 +0530

    Added UI

--- CoreML-Vision-ObjectRecognizer/ViewController.swift
+++ CoreML-Vision-ObjectRecognizer/ViewController.swift
@@ -7,11 +7,31 @@
 //
 
 import UIKit
+import MobileCoreServices
+import Vision
+import CoreML
+import AVKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var highscoreLabel: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var objectLabel: UILabel!
+    @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var skipButton: UIButton!
+    @IBOutlet weak var topView: UIView!
+    @IBOutlet weak var bottomView: UIView!
+    
+    var cameraLayer: CALayer!
+    var gameTimer: Timer!
+    var timeRemaining = 60
+    var currentScore = 0
+    var highScore = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupView()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +40,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func setupView(){
+        let backgroundColor = UIColor.init(red: 255/255, green: 255/255, blue: 255/255, alpha: 0.8)
+        topView.backgroundColor = backgroundColor
+        bottomView.backgroundColor = backgroundColor
+        scoreLabel.text = "0"
+    }
+    
 }
 
